19b2b1d9f8d3471a5976c12fce2f1184277365bc
Farhaan Nishtar <farhaannishtar@gmail.com>
2024-07-26 13:18:16 -0400

    feat: add delete functionality

--- src/components/CatList.tsx
+++ src/components/CatList.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchCats from '../hooks/useFetchCats';
 import CatCard from './CatCard';
+// import { Cat } from '../types';
 
 const CatList: React.FC = () => {
-  const { data: cats, isLoading, error } = useFetchCats();
+  const { data: initialCats, isLoading, error } = useFetchCats();
+  const [cats, setCats] = useState(initialCats);
 
   if (isLoading) return <div>Loading...</div>;
 
   if (error) return <div>Error</div>;
 
-  const handleDelete = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    console.log('Delete button clicked');
+  const handleDelete = (id: string) => {
+    setCats(cats?.filter(cat => cat.id !== id));
+    console.log(`Delete button clicked for cat with id: ${id}`);
   }
+
+  console.log(cats);
   
   return (
     <>
       <ul className="flex flex-wrap justify-center items-center">
         {cats?.map((cat) => (
-          <CatCard cat={cat} handleDelete={handleDelete}/>
+          <CatCard cat={cat} handleDelete={() => handleDelete(cat.id)}/>
         ))}
       </ul>
     </>
